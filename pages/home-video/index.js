2e450ff96d545388ca736dd7cb4cf89870cfe886
956622329 <956622329@qq.com>
2022-10-27 23:18:29 +0800

    video页面搭建

--- pages/home-video/index.js
+++ pages/home-video/index.js
@@ -6,14 +6,47 @@ Page({
    * 页面的初始数据
    */
   data: {
-    topMVs: []
+    topMVs: [],
+    hasMore: true
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  async onLoad (options) {
-    const res = await getTopMv(0)
-    this.setData({ topMVs: res.data })
+  onLoad(options) {
+    this.getTopMVData(0)
+  },
+  //封装网络请求
+  async getTopMVData(offset) {
+    //判断是否可以请求
+    if (!this.data.hasMore && offset !== 0) return
+    //请求数据
+    const res = await getTopMv(offset)
+    let newData = this.data.topMVs
+    if (offset === 0) {
+      newData = res.data
+    } else {
+      newData = newData.concat(res.data)
+    }
+
+    //设置数据
+    this.setData({ topMVs: newData })
+    this.setData({ hasMore: res.hasMore })
+    wx.stopPullDownRefresh()
+  },
+  //封装事件处理的方法
+  handleVideoItemClick(event) {
+    const id = event.currentTarget.dataset.item.id
+    wx.navigateTo({
+      url: `/pages/detail-video/index?id=${id}`,
+    })
+  },
+  //其他生命周期函数
+  onPullDownRefresh() {
+    this.getTopMVData(0)
+  },
+  onReachBottom() {
+    this.getTopMVData(this.data.topMVs.length)
   }
+
 })
